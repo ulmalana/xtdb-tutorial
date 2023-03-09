2395fccf0cfa1937c56ff492464fb5604b319b50
rz <muhamaul@gmail.com>
2023-03-09 16:39:23 +0700

    finished part 5

--- src/xtdb_tutorial.clj
+++ src/xtdb_tutorial.clj
@@ -270,3 +270,281 @@
 
 (xt/sync node)
 ;; => #inst "2023-03-08T08:33:37.331-00:00"
+
+
+;;; part 4
+
+;; use valid time
+(xt/submit-tx
+ node
+ [[::xt/put
+   {:xt/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2114-12-03"]])
+;; => #:xtdb.api{:tx-id 7, :tx-time #inst "2023-03-09T08:51:01.026-00:00"}
+
+(xt/sync node)
+;; => #inst "2023-03-09T08:51:01.026-00:00"
+
+;; bitemporality
+(xt/submit-tx
+ node
+ [[::xt/put
+   {:xt/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2113-12-03" ;; valid time start
+   #inst "2114-12-03"] ;; valid time end
+
+  [::xt/put
+   {:xt/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2112-12-03"
+   #inst "2113-12-03"]
+
+  [::xt/put
+   {:xt/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? false}
+   #inst "2112-06-03"
+   #inst "2112-12-02"]
+
+  [::xt/put
+   {:xt/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Promotional}
+   #inst "2111-06-03"
+   #inst "2112-06-03"]])
+;; => #:xtdb.api{:tx-id 8, :tx-time #inst "2023-03-09T08:58:58.959-00:00"}
+
+(xt/sync node)
+;; => #inst "2023-03-09T08:58:58.959-00:00"
+
+;; queries through time
+(xt/q
+ (xt/db node #inst "2114-01-01")
+ '{:find [cover type]
+   :where [[e :consumer-id :RJ29sUU]
+           [e :cover? cover]
+           [e :cover-type type]]})
+;; => #{[true :Full]}
+
+(xt/q
+ (xt/db node #inst "2111-07-03")
+ '{:find [cover type]
+   :where [[e :consumer-id :RJ29sUU]
+           [e :cover? cover]
+           [e :cover-type type]]})
+;; => #{[true :Promotional]}
+
+(xt/q
+ (xt/db node #inst "2112-07-03")
+ '{:find [cover type]
+   :where [[e :consumer-id :RJ29sUU]
+           [e :cover? cover]
+           [e :cover-type type]]})
+;; => #{}
+
+(xt/submit-tx
+ node
+ [[::xt/put
+   {:xt/id :manifest
+    :pilot-name "Johanna"
+    :id/rocket "SB002-sol"
+    :id/employee "22910x2"
+    :badges ["SETUP" "PUT" "DATALOG-QUERIES" "BITEMP"]
+    :cargo ["stereo" "gold fish" "slippers" "secret note"]}]])
+
+(xt/sync node)
+;; => #inst "2023-03-09T09:07:05.276-00:00"
+
+;;; part 5
+(def data-5
+  [{:xt/id :gold-harmony
+    :company-name "Gold Harmony"
+    :seller? true
+    :buyer? false
+    :units/Au 10211
+    :credits 51}
+
+   {:xt/id :tombaugh-resources
+    :company-name "Tombaugh Resources Ltd."
+    :seller? true
+    :buyer? false
+    :units/Pu 50
+    :units/N 3
+    :units/CH4 92
+    :credits 51}
+
+   {:xt/id :encompass-trade
+    :company-name "Encompass Trade"
+    :seller? true
+    :buyer? true
+    :units/Au 10
+    :units/Pu 5
+    :units/CH4 211
+    :credits 1002}
+
+   {:xt/id :blue-energy
+    :seller? false
+    :buyer? true
+    :company-name "Blue Energy"
+    :credits 1000}])
+
+(easy-ingest node data-5)
+;; => #inst "2023-03-09T09:16:42.700-00:00"
+
+;; check stock and fund level
+(defn stock-check
+  [company-id item]
+  {:result (xt/q
+            (xt/db node)
+            {:find '[name funds stock]
+             :where ['[e :company-name name]
+                     '[e :credits funds]
+                     ['e item 'stock]]
+             :in '[e]}
+            company-id)
+   :item item})
+
+(defn format-stock-check
+  [{:keys [result item] :as stock-check}]
+  (for [[name funds commod] result]
+    (str "Name: " name ", Funds: " funds ", " item " " commod)))
+
+;; move 10 units of methane for 100 credits to blue energy using match
+(xt/submit-tx
+ node
+ [[::xt/match
+   :blue-energy
+   {:xt/id :blue-energy
+    :seller? false
+    :buyer? true
+    :company-name "Blue Energy"
+    :credits 1000}]
+
+  [::xt/put
+   {:xt/id :blue-energy
+    :seller? false
+    :buyer? true
+    :company-name "Blue Energy"
+    :credits 900
+    :units/CH4 10}]
+
+  [::xt/match
+   :tombaugh-resources
+   {:xt/id :tombaugh-resources
+    :company-name "Tombaugh Resources Ltd."
+    :seller? true
+    :buyer? false
+    :units/Pu 50
+    :units/N 3
+    :units/CH4 92
+    :credits 51}]
+
+  [::xt/put
+   {:xt/id :tombaugh-resources
+    :company-name "Tombaugh Resources Ltd."
+    :seller? true
+    :buyer? false
+    :units/Pu 50
+    :units/N 3
+    :units/CH4 82
+    :credits 151}]])
+;; => #:xtdb.api{:tx-id 11, :tx-time #inst "2023-03-09T09:29:55.651-00:00"}
+
+(xt/sync node)
+;; => #inst "2023-03-09T09:29:55.651-00:00"
+
+(format-stock-check (stock-check :tombaugh-resources :units/CH4))
+;; => ("Name: Tombaugh Resources Ltd., Funds: 151, :units/CH4 82")
+
+(format-stock-check (stock-check :blue-energy :units/CH4))
+;; => ("Name: Blue Energy, Funds: 900, :units/CH4 10")
+
+;; failed transaction because match is not valid
+(xt/submit-tx
+ node
+ [[::xt/match
+   :gold-harmony
+   {:xt/id :gold-harmony
+    :company-name "Gold Harmony"
+    :seller? true
+    :buyer? false
+    :units/Au 10211
+    :credits 51}]
+  [::xt/put
+   {:xt/id :gold-harmony
+    :company-name "Gold Harmony"
+    :seller? true
+    :buyer? false
+    :units/Au 211
+    :credits 51}]
+
+  [::xt/match
+   :encompass-trade
+   {:xt/id :encompass-trade
+    :company-name "Encompass Trade"
+    :seller? true
+    :buyer? true
+    :units/Au 10
+    :units/Pu 5
+    :units/CH4 211
+    :credits 100002}]
+  [::xt/put
+   {:xt/id :encompass-trade
+    :company-name "Encompass Trade"
+    :seller? true
+    :buyer? true
+    :units/Au 10010
+    :units/Pu 5
+    :units/CH4 211
+    :credits 1002}]])
+;; => #:xtdb.api{:tx-id 12, :tx-time #inst "2023-03-09T09:34:52.367-00:00"}
+
+(xt/sync node)
+;; => #inst "2023-03-09T09:34:52.367-00:00"
+
+(format-stock-check (stock-check :gold-harmony :units/Au))
+;; => ("Name: Gold Harmony, Funds: 51, :units/Au 10211")
+
+(format-stock-check (stock-check :encompass-trade :units/Au))
+;; => ("Name: Encompass Trade, Funds: 1002, :units/Au 10")
+
+(xt/submit-tx
+ node
+ [[::xt/put
+   {:xt/id :manifest
+    :pilot-name "Johanna"
+    :id/rocket "SB002-sol"
+    :id/employee "22910x2"
+    :badges ["SETUP" "PUT" "DATALOG-QUERIES" "BITEMP" "MATCH"]
+    :cargo ["stereo" "gold fish" "slippers" "secret note"]}]])
+;; => #:xtdb.api{:tx-id 13, :tx-time #inst "2023-03-09T09:36:41.099-00:00"}
+
+(xt/sync node)
+;; => #inst "2023-03-09T09:36:41.099-00:00"
+
+(xt/q
+ (xt/db node)
+  '{:find [belongings]
+    :where [[e :cargo belongings]]
+    :in [belongings]}
+  "secret note")
+;; => #{["secret note"]}
